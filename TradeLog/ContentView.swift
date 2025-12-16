a83294266174b8fa664cef8361792360c235c358
vigneshchowdary31-debug <vigneshchowdary31@gmail.com>
2025-12-16 11:33:52 +0530

    Initial Commit

---
+++ TradeLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TradeLog
+//
+//  Created by Vignesh Chowdary on 16/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TradeLog/TradeLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeLogApp.swift
+//  TradeLog
+//
+//  Created by Vignesh Chowdary on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TradeLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
